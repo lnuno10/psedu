530387c6fe2f1faa9df055973fb855d2cf9e8d2d
lmiranda <0892628@lbcc.edu>
2022-06-07 23:57:48 -0700

    initial commit - create OUs script

---
+++ createous.ps1
@@ -0,0 +1,17 @@
+
+# import Active Directory Module
+Import-Module ActiveDirectory
+
+# Create Root OU
+# New-ADOrganizational Unit 'Corp'
+
+# Create Child OUs in Corp
+$OUs = @(
+    'Users'
+    'Groups'
+    'Computers'
+    'Servers'
+)
+foreach ($ou in $OUs) {
+    New-ADOrganizationalUnit -Path 'OU=Corp,DC=acme,DC=local' -Name $ou
+}
