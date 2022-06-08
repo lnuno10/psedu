98f36c04401f9d6c69e684ba26d9b369eb6badb6
lmiranda <0892628@lbcc.edu>
2022-06-07 23:56:53 -0700

    csv ad user script functional

---
+++ createusers.ps1
@@ -0,0 +1,37 @@
+git config --global user.email "0892628@lbcc.edu"
+git config --global user.name "lmiranda"
+# import Active Directory Module
+Import-Module ActiveDirectory
+
+# Read in Secure Temporary Password (Note: Complexity)
+$AccountPassword = Read-Host -AsSecureString -Prompt "Enter Temporary Password for New Users"
+
+# Important User Data
+$csv = Import-Csv -Path C:\Users\lmiranda\Documents\adusers.csv
+
+# Loop through Users
+foreach ($User in $csv) {
+
+    # Store User Attributes for Splatting
+    $UserInfo = @{	
+        Name = $user.Name
+        DisplayName = $User.Name
+        GivenName = $User.GivenName
+        Surname = $User.Surname
+
+        SamAccountName = $User.SamAccountName
+        Path = $user.Path
+
+        Title = $User.Title
+        Department = $User.Department
+        Company = $User.Company
+        EmailAddress = $User.EmailAddress
+
+        AccountPassword = $AccountPassword
+        ChangePasswordAtLogon = $true
+        Enabled = $true
+    }
+
+    # Create New Users
+    New-ADUser @UserInfo
+}
